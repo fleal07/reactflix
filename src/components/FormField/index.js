1e1b795ea785cacfbc21cc6a441e1ca00bcf6fbe
fleal07 <fleal07@gmail.com>
2020-07-31 05:57:46 -0300

    Início form do cadastro de categorias usando state

---
+++ src/components/FormField/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function FormField({ label, type, name, value, onChange }) {
+  return (
+    <div>
+      <label>
+        Nome da Categoria:
+        <input
+          label={label}
+          type={type}
+          value={value}
+          name={name}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
+}
+
+export default FormField;
